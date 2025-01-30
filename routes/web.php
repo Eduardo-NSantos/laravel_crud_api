0efb5b3b7b4fbfde4753ce2527040bb649a3e5e3
Eduardo-SNantos <eduardoshw123@gmail.com>
2025-01-30 20:56:38 -0300

    criação do controller e rotas para clients

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     abort(403, 'Unauthorized access');
 });
+
+Route::apiResource('/clients', ClientController::class);
