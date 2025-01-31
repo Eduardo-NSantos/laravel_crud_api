bfa417916535070ba6bf593d11cfbd4d7feef0f3
Eduardo-SNantos <eduardoshw123@gmail.com>
2025-01-31 14:11:22 -0300

    listagem de todos os clientes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     abort(403, 'Unauthorized access');
 });
-
-Route::apiResource('/clients', ClientController::class);
